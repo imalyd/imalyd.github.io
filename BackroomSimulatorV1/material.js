f3edfb42f6e29ffe9d51c5eea3779dda37938422
imalyd <thomas.gu@qq.com>
2022-03-16 17:27:58 +0800

    update backroomsimulatorv1

--- BackroomSimulatorV1/material.js
+++ BackroomSimulatorV1/material.js
@@ -1,38 +1,24 @@
-
-				var materialCount = 0;
-				var getMaterialName = function () {
-					return "m" + ++materialCount;
-				};
-				var createMaterial=function(imagePath,scene){
-					var material= new BABYLON.StandardMaterial(
-						getMaterialName(),
-						scene
-					);
-					material.diffuseTexture = new BABYLON.Texture(
-						imagePath,
-						scene
-					);
-					return material;
-				}
-				var createEmissiveMaterial=function(imagePath,scene){
-					var material= new BABYLON.StandardMaterial(
-						getMaterialName(),
-						scene
-					);
-					material.emissiveTexture = new BABYLON.Texture(
-						imagePath,
-						scene
-					);
-					return material;
-				}
-
-/*
-					var level0FloorMaterial = new BABYLON.StandardMaterial(
-						"level0FloorMaterial",
-						scene
-					);
-					level0FloorMaterial.diffuseTexture = new BABYLON.Texture(
-						"./level0_floor.jpg",
-						scene
-					);
-					*/
+var materialCount = 0;
+var getMaterialName = function () {
+	return "m" + ++materialCount;
+};
+var createMaterial = function (imagePath, scene) {
+	var material = new BABYLON.StandardMaterial(getMaterialName(), scene);
+	material.diffuseTexture = new BABYLON.Texture(imagePath, scene);
+	return material;
+};
+var createEmissiveMaterial = function (imagePath, scene) {
+	var material = new BABYLON.StandardMaterial(getMaterialName(), scene);
+	material.emissiveTexture = new BABYLON.Texture(imagePath, scene);
+	return material;
+};
+var createMaterialFromColor = function (r, g, b, scene) {
+	var material = new BABYLON.StandardMaterial(getMaterialName(), scene);
+	material.diffuseColor = new BABYLON.Color3(r, g, b);
+	return material;
+};
+var createEmissiveMaterialFromColor = function (r, g, b, scene) {
+	var material = new BABYLON.StandardMaterial(getMaterialName(), scene);
+	material.emissiveColor = new BABYLON.Color3(r, g, b);
+	return material;
+};
